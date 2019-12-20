d3364c2ac421d4c9eafc2f4231543ec621a9ac43
elacalle <elacalle@nosolosoftware.es>
2019-12-20 01:59:48 +0100

    Implement lodash compose
    Implement _.compose() to be able to use the return of each functions and use it in
    the next function iteration

---
+++ ex16_functional_js.js
@@ -0,0 +1,27 @@
+_ = {};
+
+_.compose = (...objects) => {
+  var functionList = objects;
+
+  var comp = (...args) => {
+    lastFunction = functionList.pop();
+    if (functionList.length == 0) return lastFunction(...args);
+    return comp(lastFunction(...args))
+  }
+
+  return comp;
+};
+
+//Compose examples
+
+const add = (a, b) => { return a + b }
+const square = (x) => { return x * x }
+
+customFunc = _.compose(square, add);
+console.log(customFunc(1,2));
+
+const greeting = (name) => { return `See you spacecowboy: ${name}`; }
+const name = (name) => { return name; }
+
+composeName = _.compose(greeting, name);
+console.log(composeName('Dance Monkey'));
